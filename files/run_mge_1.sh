c0c9dedb64b44f3b532a05a498d7f0d40db1da46
felikcat <29991266+felikcat@users.noreply.github.com>
2025-08-29 03:41:58 -0700

    Correct the game server service & run script

--- files/run_mge_1.sh
+++ files/run_mge_1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-screen -dmS "tf2-mge-1" "/home/steam/tf2_server/srcds_run_64" -console -usercon +ip YOUR_GAME_SERVERS_EXTERNAL_IP -game tf -insecure +map mge_training_v8_beta4b +servercfgfile "server_mge_1.cfg" +sv_setsteamaccount YOUR_STEAM_GLST_KEY_HERE -port 27015 -autoupdate -steamcmd_script "/home/steam/tf2_autoupdate.txt" -steam_dir "/home/steam/tf2_server"
+screen -DmS "tf2-mge-1" "/home/steam/tf2_server/srcds_run_64" -console -usercon +ip YOUR_GAME_SERVERS_EXTERNAL_IP -game tf -insecure +map mge_training_v8_beta4b +servercfgfile "server_mge_1.cfg" +sv_setsteamaccount YOUR_STEAM_GLST_KEY_HERE -port 27015 -autoupdate -steamcmd_script "/home/steam/tf2_autoupdate.txt" -steam_dir "/home/steam/tf2_server"
